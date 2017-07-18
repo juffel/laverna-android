825d0dba91c41fc7c2bd8aab9328c83d25524b70
Julian Dobmann <julian.dobmann@mailbox.org>
2017-07-19 00:18:34 +0200

    WIP get file upload partially working;
    At least the MyWebChromeClient's callback functions are hit when
    selecting & uploading a file from android file storage.

--- app/src/main/java/space/juffel/laverna/MainActivity.java
+++ app/src/main/java/space/juffel/laverna/MainActivity.java
@@ -1,10 +1,16 @@
 package space.juffel.laverna;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWebView = (WebView) findViewById(R.id.activity_main_webview);
-
         WebView.setWebContentsDebuggingEnabled(true);
 
         WebSettings webSettings = mWebView.getSettings();
@@ -24,9 +29,52 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setAllowFileAccessFromFileURLs(true);
         webSettings.setAllowUniversalAccessFromFileURLs(true);
         webSettings.setDomStorageEnabled(true);
+
         // Force links and redirects to open in the WebView instead of in a browser
-        mWebView.setWebViewClient(new WebViewClient());
+        // mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new MyWebChromeClient(this));
 
         mWebView.loadUrl("file:///android_asset/index.html");
     }
+
+    // plug in file upload handler
+    // inspired by this SO-answer: https://stackoverflow.com/a/29545290/1870317
+    public static final int REQUEST_SELECT_FILE = 100;
+    public ValueCallback<Uri[]> uploadMessage;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == REQUEST_SELECT_FILE) {
+            if (uploadMessage == null) return;
+            uploadMessage.onReceiveValue(WebChromeClient.FileChooserParams.parseResult(resultCode, data));
+            uploadMessage = null;
+        }
+    }
+    public class MyWebChromeClient extends WebChromeClient {
+        private MainActivity myActivity;
+
+        public MyWebChromeClient(MainActivity activity) {
+            myActivity = activity;
+        }
+
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            // make sure there is no existing message
+            if (myActivity.uploadMessage != null) {
+                myActivity.uploadMessage.onReceiveValue(null);
+                myActivity.uploadMessage = null;
+            }
+
+            myActivity.uploadMessage = filePathCallback;
+
+            Intent intent = fileChooserParams.createIntent();
+            try {
+                myActivity.startActivityForResult(intent, MainActivity.REQUEST_SELECT_FILE);
+            } catch (ActivityNotFoundException e) {
+                myActivity.uploadMessage = null;
+                Toast.makeText(myActivity, "Cannot open file chooser", Toast.LENGTH_LONG).show();
+                return false;
+            }
+
+            return true;
+        }
+    }
+    // end plug in file upload handler
 }
